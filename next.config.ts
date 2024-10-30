69f1b58e4699566585c1f374a7228425cbeb05b2
RickWang-wh <71516640+RickWang-wh@users.noreply.github.com>
2024-10-30 08:34:54 +0000

    Add allowOrigins config in the next.config.ts, the content contain not only the host address using but the localhost:3000 should be included. And after creating new invoice, redirect to /dashboard/invoices to show the results

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next';
 
+
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["vigilant-barnacle-g6vwvp47j7r2xx-3000.app.github.dev", "localhost:3000"],
+    },
+  },
 };
-
+ 
 export default nextConfig;
